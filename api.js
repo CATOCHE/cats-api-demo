114fbd3cb9e505794710845afc5bda2d1ebc5c13
CATOCHE <mrcatoche@gmail.com>
2018-02-05 15:49:15 -0500

    Search implemented

--- api.js
+++ api.js
@@ -4,6 +4,7 @@ const app = express()
 const HTTPError = require('node-http-error')
 const bodyPaser = require('body-parser')
 const port = process.env.PORT || 5000
+const {pathOr, compose, filter, split, last, head, toLower} = require('ramda')
 
 const cats = [
   {name: "Mittens", age: 3, gender: "Female", breed: "Tabby"},
@@ -13,6 +14,34 @@ const cats = [
 ]
 
 app.get('/', (req,res) => res.send('MEOW.'))
+
+// http://localhost:5000/cats?filter=breed:Tabby
+
+app.get('/cats', (req,res)=>{
+// 'query' and 'filter' are properties of the req object that contains
+// the request from the user...
+// pathOr is taking req and returning the query and filter properties
+// if there are none, then 'no filter' will be used.
+  const queryFilter = pathOr('no filter', ['query','filter'], req)
+  if (queryFilter === 'no filter'){
+    res.send(cats)
+  } else {
+    const searchProp = compose(head, split(':')) (queryFilter)   // e.g.'breed'
+    const searchValue = toLower(compose(last, split(':')) (queryFilter))  // e.g. 'Tabby'
+
+  const catFilter = cat => {
+    if (Number.isNaN(Number(searchValue))) {
+      console.log('searchValue isNaN', searchValue)
+      return (toLower(cat[searchProp]) == searchValue)
+    } else {
+      console.log('searchValue is a Number', searchValue)
+      return cat[searchProp] == searchValue
+    }
+  }
+  res.send(filter(catFilter, cats))
+}
+})
+
 
 app.get('/cats', (req,res)=>res.send(cats))
 
