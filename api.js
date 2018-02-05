dc93a9c104d3e51e3c812fd9a11f6de4faeab52a
CATOCHE <mrcatoche@gmail.com>
2018-02-05 16:35:13 -0500

    POST implemented

--- api.js
+++ api.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 const express = require ('express')
 const app = express()
 const HTTPError = require('node-http-error')
-const bodyPaser = require('body-parser')
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 5000
-const {pathOr, compose, filter, split, last, head, toLower} = require('ramda')
+const {not, pathOr, compose, filter, split, last, head, toLower, append, propOr, isEmpty} = require('ramda')
 
 const cats = [
   {name: "Mittens", age: 3, gender: "Female", breed: "Tabby"},
@@ -13,9 +13,15 @@ const cats = [
   {name: "Miss Krunkles", age: 5, gender: "Female", breed: "Torr"},
 ]
 
+app.use(bodyParser.json())
+
 app.get('/', (req,res) => res.send('MEOW.'))
 
 // http://localhost:5000/cats?filter=breed:Tabby
+// req.params
+// req.query
+// req.body
+
 
 app.get('/cats', (req,res)=>{
 // 'query' and 'filter' are properties of the req object that contains
@@ -41,8 +47,34 @@ app.get('/cats', (req,res)=>{
   res.send(filter(catFilter, cats))
 }
 })
-
+// the body of the POST will come in this format
+// {
+//     "name": "Toodles",
+//     "age": 3,
+//     "gender": "Female",
+//     "breed": "Tabby"
+// }
+app.post('/cats/', (req, res, next) => {
+  // look for a 'body' property in the req object and return null if not found
+    const body = propOr( {}, 'body', req)
+    if (not(isEmpty(body))) {
+      // add the cat
+      res.send(append(body, cats))
+    } else {
+      next(new HTTPError(400, 'Missing cat resource in request body.'))
+    }
+})
 
 app.get('/cats', (req,res)=>res.send(cats))
+
+
+app.use(function(err, req, res, next) {
+  console.log( req.method, req.path, 'error: ', err)
+  next(err)
+})
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500)
+    res.send({status:err.status || 500, message: err.message})
+})
 
 app.listen(port, (req,res) => console.log('CATS running on port:!', port))
