3b778f3c504bf60097d2bbfabe7f537d2ffd48a1
CATOCHE <mrcatoche@gmail.com>
2018-02-05 14:15:48 -0500

    GET /cats

--- api.js
+++ api.js
@@ -5,6 +5,15 @@ const HTTPError = require('node-http-error')
 const bodyPaser = require('body-parser')
 const port = process.env.PORT || 5000
 
+const cats = [
+  {name: "Mittens", age: 3, gender: "Female", breed: "Tabby"},
+  {name: "Muffins", age: 2, gender: "Male", breed: "Siamese"},
+  {name: "Mr. Handsome", age: 8, gender: "Male", breed: "Tom"},
+  {name: "Miss Krunkles", age: 5, gender: "Female", breed: "Torr"},
+]
+
 app.get('/', (req,res) => res.send('MEOW.'))
+
+app.get('/cats', (req,res)=>res.send(cats))
 
 app.listen(port, (req,res) => console.log('CATS running on port:!', port))
